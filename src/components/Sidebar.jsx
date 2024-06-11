c125d2cb4046948577c3236d36a89d8fbdfb36ee
guna shekar <21pa1a0540@vishnu.edu.in>
2024-06-11 22:17:23 +0530

    api completed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,21 +1,23 @@
-import { Link } from "react-router-dom"
-import { Heart,Home } from "lucide-react"
+import { Heart, Home } from "lucide-react";
+import { Link } from "react-router-dom";
+
 const Sidebar = () => {
-  return (
-    <>
-      <DesktopSidebar />
-      <MobileSidebar />
-    </>
-  )
-}
-export default Sidebar
+	return (
+		<>
+			<DesktopSidebar />
+			<MobileSidebar />
+		</>
+	);
+};
+export default Sidebar;
+
 const DesktopSidebar = () => {
 	return (
 		<div className='p-3 md:p-10 border-r min-h-screen w-24 md:w-64 hidden sm:block'>
 			<div className='flex flex-col gap-20 sticky top-10 left-0'>
 				<div className='w-full'>
-					<img src='/logo.svg' alt='logo' className='hidden md:block' />
-					<img src='/mobile-logo.svg' alt='logo' className='block md:hidden' />
+					<img src='/recipe-logo.jpg' alt='logo' className='hidden md:block' />
+					<img src='/mobile-logo.jpeg' alt='logo' className='block md:hidden' />
 				</div>
 				<ul className='flex flex-col items-center md:items-start gap-8'>
 					<Link to={"/"} className='flex gap-1'>
@@ -31,6 +33,7 @@ const DesktopSidebar = () => {
 		</div>
 	);
 };
+
 const MobileSidebar = () => {
 	return (
 		<div
